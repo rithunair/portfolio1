a17b3dc9b99adc47f27f7837fa1b3490dd59850e
rithunair <38644598+rithunair@users.noreply.github.com>
2018-09-06 12:27:46 +0530

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,26 @@
+/*============================================================
+                        SERVICE
+=============================================================*/
+$(function () {
+    new WOW().init();
+});
+/*============================================================
+                        WORK (FOR PORTIFOLIO PAGE)
+=============================================================*/
+/*$(function)() {
+        $("#work").magnificPopup({
+            delegate: 'a', // child items selector, by clicking on it popup will open
+            type: 'image'
+        });
+    });*/
+
+
+$(function() {
+ 
+    $("#work").magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image'
+  // other options
+});
+    
+});
